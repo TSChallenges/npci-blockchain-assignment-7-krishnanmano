c0116b0b56ad9e1217381115cb0136d201279e6d
Manoj Krishnan <8068772+krishnanmano@users.noreply.github.com>
2025-02-27 18:27:52 +0000

    assignment 7 completed

--- chaincode/erc20token.go
+++ chaincode/erc20token.go
@@ -1,48 +1,235 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+// Token represents the structure of the token state
 type Token struct {
-	Name   string `json:"name"`
-	Symbol string `json:"symbol"`
-	Decimals int    `json:"decimals"`
-	TotalSupply int `json:"totalSupply"`
+	Name        string `json:"name"`
+	Symbol      string `json:"symbol"`
+	Decimals    int    `json:"decimals"`
+	TotalSupply int    `json:"totalSupply"`
 }
 
+// TokenContract provides functions for managing the token
 type TokenContract struct {
 	contractapi.Contract
 }
 
+// Balance reflects the balance of a user
 type Balance struct {
 	Balance int `json:"balance"`
 }
 
+type Approval struct {
+	Owner     string `json:"owner"`
+	Spender   string `json:"spender"`
+	Allowance int    `json:"allowance"`
+}
 
 // TODO: InitLedger for token initialization
+func (t *TokenContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", "UserA")
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: user should be admin")
+	}
 
+	token := Token{
+		Name:        "BNB-Token",
+		Symbol:      "BNB",
+		Decimals:    18,
+		TotalSupply: 200000000,
+	}
+	tokenAsBytes, _ := json.Marshal(token)
+
+	err = ctx.GetStub().PutState("token", tokenAsBytes)
+	if err != nil {
+		return fmt.Errorf("failed to put to world state. %v", err)
+	}
+
+	// Initialize admin balance
+	admin := Balance{Balance: token.TotalSupply}
+	adminAsBytes, _ := json.Marshal(admin)
+	return ctx.GetStub().PutState("UserA", adminAsBytes)
+}
 
 // TODO: MintTokens for minting tokens
+func (t *TokenContract) MintTokens(ctx contractapi.TransactionContextInterface, amount int) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", "UserA")
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: user should be admin")
+	}
 
+	adminAsBytes, err := ctx.GetStub().GetState("UserA")
+	if err != nil {
+		return err
+	}
+
+	var admin Balance
+	json.Unmarshal(adminAsBytes, &admin)
+
+	admin.Balance += amount
+	updatedAdminAsBytes, _ := json.Marshal(admin)
+	return ctx.GetStub().PutState("UserA", updatedAdminAsBytes)
+}
 
 // TODO: TransferTokens for transferring tokens
+func (t *TokenContract) TransferTokens(ctx contractapi.TransactionContextInterface, from string, to string, amount int) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", from)
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: calling user should be sender")
+	}
 
+	fromAsBytes, _ := ctx.GetStub().GetState(from)
+	toAsBytes, _ := ctx.GetStub().GetState(to)
+
+	var fromBalance, toBalance Balance
+	json.Unmarshal(fromAsBytes, &fromBalance)
+	json.Unmarshal(toAsBytes, &toBalance)
+
+	if fromBalance.Balance < amount {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	fromBalance.Balance -= amount
+	toBalance.Balance += amount
+
+	updatedFromAsBytes, _ := json.Marshal(fromBalance)
+	updatedToAsBytes, _ := json.Marshal(toBalance)
+	ctx.GetStub().PutState(from, updatedFromAsBytes)
+	ctx.GetStub().PutState(to, updatedToAsBytes)
+
+	return nil
+}
 
 // TODO: GetBalance to check the balance
+func (t *TokenContract) GetBalance(ctx contractapi.TransactionContextInterface, owner string) (int, error) {
+	ownerAsBytes, _ := ctx.GetStub().GetState(owner)
 
+	var ownerBalance Balance
+	json.Unmarshal(ownerAsBytes, &ownerBalance)
+
+	return ownerBalance.Balance, nil
+}
 
 // TODO: ApproveSpender for approving spending
+func (t *TokenContract) ApproveSpender(ctx contractapi.TransactionContextInterface, owner string, spender string, amount int) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", owner)
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: calling user should be owner")
+	}
 
+	// Create an approval object
+	approval := Approval{
+		Owner:     owner,
+		Spender:   spender,
+		Allowance: amount,
+	}
+
+	approvalAsBytes, err := json.Marshal(approval)
+	if err != nil {
+		return err
+	}
+
+	// Use a composite key to uniquely identify the allowance for a spender by an owner
+	approvalKey, err := ctx.GetStub().CreateCompositeKey("approval", []string{owner, spender})
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(approvalKey, approvalAsBytes)
+}
 
 // TODO: TransferFrom for transferring from approved spenders
+func (t *TokenContract) TransferFrom(ctx contractapi.TransactionContextInterface, owner string, spender string, recipient string, amount int) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", spender)
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: calling user should be spender")
+	}
 
+	// Retrieve the approval to check if the spender is allowed to spend tokens
+	approvalKey, err := ctx.GetStub().CreateCompositeKey("approval", []string{owner, spender})
+	if err != nil {
+		return err
+	}
+
+	approvalAsBytes, err := ctx.GetStub().GetState(approvalKey)
+	if err != nil {
+		return err
+	}
+
+	var approval Approval
+	if err := json.Unmarshal(approvalAsBytes, &approval); err != nil {
+		return err
+	}
+
+	if approval.Allowance < amount {
+		return fmt.Errorf("spender does not have enough allowance")
+	}
+
+	// Decrease the allowance
+	approval.Allowance -= amount
+	updatedApprovalAsBytes, err := json.Marshal(approval)
+	if err != nil {
+		return err
+	}
+
+	ctx.GetStub().PutState(approvalKey, updatedApprovalAsBytes)
+
+	// Proceed with transferring tokens from owner to recipient
+	ownerAsBytes, err := ctx.GetStub().GetState(owner)
+	if err != nil {
+		return err
+	}
+
+	recipientAsBytes, err := ctx.GetStub().GetState(recipient)
+	if err != nil {
+		return err
+	}
+
+	var ownerBalance, recipientBalance Balance
+	json.Unmarshal(ownerAsBytes, &ownerBalance)
+	json.Unmarshal(recipientAsBytes, &recipientBalance)
+
+	if ownerBalance.Balance < amount {
+		return fmt.Errorf("owner does not have enough balance")
+	}
+
+	// Transfer the tokens
+	ownerBalance.Balance -= amount
+	recipientBalance.Balance += amount
+
+	updatedOwnerAsBytes, _ := json.Marshal(ownerBalance)
+	updatedRecipientAsBytes, _ := json.Marshal(recipientBalance)
+	ctx.GetStub().PutState(owner, updatedOwnerAsBytes)
+	ctx.GetStub().PutState(recipient, updatedRecipientAsBytes)
+
+	return nil
+}
 
 // TODO: BurnTokens for burning tokens
+func (t *TokenContract) BurnTokens(ctx contractapi.TransactionContextInterface, amount int) error {
+	err := ctx.GetClientIdentity().AssertAttributeValue("user", "UserA")
+	if err != nil {
+		return fmt.Errorf("unauthorized operation: user should be admin")
+	}
 
+	adminAsBytes, err := ctx.GetStub().GetState("UserA")
+	if err != nil {
+		return err
+	}
+
+	var admin Balance
+	json.Unmarshal(adminAsBytes, &admin)
+
+	admin.Balance -= amount
+	updatedAdminAsBytes, _ := json.Marshal(admin)
+	return ctx.GetStub().PutState("UserA", updatedAdminAsBytes)
+}
 
 func main() {
 	chaincode, err := contractapi.NewChaincode(&TokenContract{})
@@ -55,4 +242,3 @@ func main() {
 		fmt.Printf("Error starting token chaincode: %v", err)
 	}
 }
-
--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -1,18 +1,85 @@
 #!/bin/bash
 
+## Note: prereqs
+# 1. Clone `fabric-samples` repo
+# 2. Move `chaincode` directory to `fabric-samples/erc20token/chaincode/`
+# 3. Move this script to `fabric-samples/test-network` directory
+
+./network.sh down
+
 CHANNEL_NAME=mychannel
 CHAINCODE_NAME=erc20token
 CHAINCODE_VERSION=1.0
 CHAINCODE_PATH=github.com/erc20token
 
+
+./network.sh up createChannel -ca
+
+./network.sh deployCC -ccn erc20token -ccp ../erc20token/chaincode/ -ccl go -ccv 1.0
+
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/org1.example.com/
+
+fabric-ca-client register --id.name UserA --id.secret userapw --id.type client --id.affiliation org1 --id.attrs 'user=UserA:ecert' --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+fabric-ca-client enroll -u https://UserA:userapw@localhost:7054 --caname ca-org1 -M "${PWD}/organizations/peerOrganizations/org1.example.com/users/UserA@org1.example.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+cp "${PWD}/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/org1.example.com/users/UserA@org1.example.com/msp/config.yaml"
+
+
+fabric-ca-client register --id.name User2 --id.secret user2pw --id.type client --id.affiliation org1 --id.attrs 'user=User2:ecert' --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+fabric-ca-client enroll -u https://User2:user2pw@localhost:7054 --caname ca-org1 -M "${PWD}/organizations/peerOrganizations/org1.example.com/users/User2@org1.example.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+cp "${PWD}/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/org1.example.com/users/User2@org1.example.com/msp/config.yaml"
+
+fabric-ca-client register --id.name User3 --id.secret user3pw --id.type client --id.affiliation org1 --id.attrs 'user=User3:ecert' --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+fabric-ca-client enroll -u https://User3:user3pw@localhost:7054 --caname ca-org1 -M "${PWD}/organizations/peerOrganizations/org1.example.com/users/User3@org1.example.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+cp "${PWD}/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/org1.example.com/users/User3@org1.example.com/msp/config.yaml"
+
+fabric-ca-client register --id.name User4 --id.secret user4pw --id.type client --id.affiliation org1 --id.attrs 'user=User4:ecert' --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+fabric-ca-client enroll -u https://User4:user4pw@localhost:7054 --caname ca-org1 -M "${PWD}/organizations/peerOrganizations/org1.example.com/users/User4@org1.example.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/org1/tls-cert.pem"
+
+cp "${PWD}/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/org1.example.com/users/User4@org1.example.com/msp/config.yaml"
+
 # Set the environment variables for Org1
+export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID=Org1MSP
-export CORE_PEER_MSPCONFIGPATH=../organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_ADDRESS=localhost:7051
+export TARGET_TLS_OPTIONS=(-o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt")
+
+# InitLedger
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/UserA@org1.example.com/msp
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"function":"InitLedger","Args":[]}'
 
 # Invoke minting
-# TODO
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"function":"MintTokens","Args":["10000"]}'
 
+# Get Admin Balance
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"Args":["GetBalance", "UserA"]}'
 
 # Invoke transfer tokens
-# TODO
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"function":"TransferTokens","Args":["UserA", "User2", "100"]}'
+
+# Get User2 Balance
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/User2@org1.example.com/msp
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"Args":["GetBalance", "User2"]}'
+
+# Approve Token Spend
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"Args":["ApproveSpender", "User2", "User3", "20"]}'
+
+# TransferFrom User2 from Approved Spender User3
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/User3@org1.example.com/msp
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"function":"TransferFrom","Args":["User2", "User3", "User4", "10"]}'
+
+# GetBalance for User4
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"Args":["GetBalance", "User4"]}'
+
+# BurnTokens
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/UserA@org1.example.com/msp
+peer chaincode invoke "${TARGET_TLS_OPTIONS[@]}" -C mychannel -n erc20token -c '{"function":"BurnTokens","Args":["500"]}'
